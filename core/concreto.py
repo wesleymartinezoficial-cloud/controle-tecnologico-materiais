adb217f1d22087dd024614a05a7c3a49d63e98c2
wesleymartinezoficial-cloud <wesleymartinezoficial@gmail.com>
2026-08-16 10:53:01 -0300

    Update concreto.py

--- core/concreto.py
+++ core/concreto.py
@@ -1,17 +1,17 @@
 import math
 import pandas as pd
 
-def calcular_fck_estimado(cargas_kn: list, diametro_cm: float) -> dict:
-    """
-    Calcula a resistência de ruptura e o fck estimado conforme NBR 12655 (Amostragem Parcial).
-    """
-    area_cm2 = (math.pi * (diametro_cm**2)) / 4.0
-    fcs = [(carga * 1000.0 / area_cm2) / 10.0 for carga in cargas_kn]
-
-    n = len(fcs)
+def calcular_fck_estimado(df_cps: pd.DataFrame, diametro_cm: float) -> dict:
+    if diametro_cm <= 0:
+        raise ValueError("O diâmetro deve ser positivo.")
+    
+    cargas = df_cps["Carga (kN)"].tolist()
+    n = len(cargas)
     if n < 2:
         return None
 
+    area_cm2 = (math.pi * (diametro_cm**2)) / 4.0
+    fcs = [(carga * 1000.0 / area_cm2) / 10.0 for carga in cargas]
     fcs_ordenados = sorted(fcs)
     fc_m = sum(fcs) / n
 
@@ -21,9 +21,13 @@ def calcular_fck_estimado(cargas_kn: list, diametro_cm: float) -> dict:
     fck_est_2 = 0.85 * fc_m
     fck_est = max(fck_est_1, fck_est_2)
 
+    df_res = df_cps.copy()
+    df_res["fc (MPa)"] = [round(x, 2) for x in fcs]
+
     return {
-        "fcs": fcs,
+        "df_resultado": df_res,
         "fc_m": fc_m,
         "fck_est": fck_est,
-        "area_cm2": area_cm2,
+        "f1": fcs_ordenados[0],
+        "area_cm2": area_cm2
     }
